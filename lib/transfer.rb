d199d9a13c1261da70860e34f771bb5b27de4fec
Josh Cesana <joshuacesana@gmail.com>
2018-05-26 19:26:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
   
@@ -11,6 +12,7 @@ class Transfer
   
   def valid?
     sender.valid? && receiver.valid? && sender.balance >= amount 
+    binding.pry 
   end 
   
   
